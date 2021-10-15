92f6b1ccc4355cd58e355bca1b1a33d964611986
= <22709469@student.uwa.edu.au>
2021-10-15 18:09:14 +0800

    Fix s3_policy for site iam

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -5,12 +5,18 @@ resource "aws_iam_user" "iam_user" {
 data "aws_iam_policy_document" "s3_policy" {
   statement {
     actions   = ["s3:*"]
-    resources = [aws_s3_bucket.s3.arn]
+    resources = [
+      aws_s3_bucket.s3.arn,
+      "${aws_s3_bucket.s3.arn}/*"
+    ]
     effect    = "Allow"
   }
   statement {
-    actions   = ["s3:*"]
-    resources = ["arn:aws:s3:::${data.terraform_remote_state.remote.config.bucket}"]
+    actions   = ["s3:GetObject", "s3:PutObject"]
+    resources = [
+      "arn:aws:s3:::${data.terraform_remote_state.remote.config.bucket}",
+      "arn:aws:s3:::${data.terraform_remote_state.remote.config.bucket}/*"
+    ]
     effect    = "Allow"
   }
 }
