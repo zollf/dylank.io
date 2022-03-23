3486c3c36b00a726d9353e8621259b0bcffa30f5
Dylan Kennedy <dylankennedy2000@hotmail.com>
2022-03-23 20:07:29 +0800

    Style updates

--- frontend/src/components/Tag/index.tsx
+++ frontend/src/components/Tag/index.tsx
@@ -6,17 +6,15 @@ import styles from './styles.module.scss';
 interface Props {
   onClick?: () => void;
   children: React.ReactNode;
-  small?: boolean;
   active?: boolean;
 }
 
-export default function Tag({ children, onClick, small, active }: Props) {
+export default function Tag({ children, onClick, active }: Props) {
   return (
     <button
       type="button"
       className={cc({
         [styles.tag]: true,
-        [styles.small]: small,
         [styles.hasFunc]: !!onClick,
         [styles.active]: active,
       })}
